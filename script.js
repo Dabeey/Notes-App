0534d075aef33b5b11927d1827072ecb1225392c
Dabeey <abbasdaughter18@gmail.com>
2025-06-12 17:27:45 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const notesContainer = document.querySelector('.notes-container');
+const createBtn = document.querySelector('.create-btn');
+
+updateStorage = () => {
+    const notes = Array.from(notesContainer.querySelectorAll('.input-box')).map(
+        note => note.innerHTML
+    );
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+addNoteListeners = (noteDiv) => {
+    const inputBox = noteDiv.querySelector('.input-box');
+    const deleteImg = noteDiv.querySelector('img');
+    inputBox.addEventListener('keyup', updateStorage);
+    deleteImg.addEventListener('click', () => {
+        noteDiv.remove();
+        updateStorage();
+    });
+}
+
+createNote = (content = '') => {
+    const noteDiv = document.createElement('div');
+    noteDiv.className = 'note';
+
+    const inputBox = document.createElement('p');
+    inputBox.className = 'input-box';
+    inputBox.setAttribute('contenteditable', 'true');
+    inputBox.innerHTML = content;
+
+    const deleteImg = document.createElement('img');
+    deleteImg.src = 'images/delete.png';
+    deleteImg.alt = 'Delete Note';
+
+    noteDiv.appendChild(inputBox);
+    noteDiv.appendChild(deleteImg);
+    notesContainer.appendChild(noteDiv);
+
+    addNoteListeners(noteDiv);
+}
+
+showNotes = () => {
+    notesContainer.innerHTML = '';
+    const saved = localStorage.getItem('notes');
+    if (saved) {
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = saved;
+        tempDiv.querySelectorAll('.note').forEach(noteDiv => {
+            createNote(noteDiv.querySelector('.input-box').innerHTML);
+        });
+    }
+}
+
+showNotes();
+
+
+createBtn.addEventListener('click', () => {
+    createNote();
+    updateStorage();
+});
